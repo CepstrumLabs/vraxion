8b1c550013755bd78436a9d0d8c09718fa28fe6f
Michael Karotsieris <michael.karotsieris@gmail.com>
2021-08-02 21:17:16 +0100

    ignore disst and build

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,62 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import io
+import os
 
-setup(name='vraxion',
-      version='0.0.3',
-      description='A web framework written in python',
-      author='Mikeli Karotsieri',
-      author_email='michael.karotsieris@gmail.com',
-      packages=['vraxion'],
-      url="https://github.com/michael-karotsieris/vraxion"
-     )
+from setuptools import find_packages, setup
+
+NAME = "vraxion"
+DESCRIPTION = "Vraxion is a Python Web Framework built for learning purposes."
+EMAIL = "michael.karotsieris@gmail.com"
+AUTHOR = "Michael Karotsieris"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "0.0.3"
+
+REQUIRED = [
+    "Jinja2==2.10.3",
+    "parse==1.12.1",
+    "requests==2.22.0",
+    "requests-wsgi-adapter==0.4.1",
+    "WebOb==1.8.5",
+    "whitenoise==4.1.4",
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=["test_*"]),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ],
+    setup_requires=["wheel"],
+)
